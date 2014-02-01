8e7d92cfd63ba57b1fea782e2b778a95087edec9
Eugene Korbut <me@mvl.ru>
2014-02-01 21:51:00 +0700

    Update sphinx server

--- recipes/sphinx.rb
+++ recipes/sphinx.rb
@@ -1,5 +1,15 @@
 if node.app[:sphinx]
-  package 'sphinxsearch'
+  apt_repository "sphinx-beta" do
+    uri "http://ppa.launchpad.net/builds/sphinxsearch-beta/ubuntu "
+    distribution 'precise'
+    components ["main"]
+    keyserver "keyserver.ubuntu.com"
+    key "16932B16"
+  end
+
+  package 'sphinxsearch' do
+    action :upgrade
+  end
 
   # install mysql client (sphinx)
   include_recipe 'mysql::client'
